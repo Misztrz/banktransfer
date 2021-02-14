ff5b50951e9c5bb205373d93b4c9617dfb560531
jestem&pro <dominikgrochowski123@wp.pl>
2021-02-14 08:45:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
-/*if logged in via employee goes to emp workspace
- * if logged in via atm pin goes to ATM workspace
- * */
-require 'server_connect.inc.php';
-require 'get_logged.inc.php';
 
-	if(loginemp()){
-		require 'emp_wspace.php';
-		}
-    
-	else if(loginatm()){
-		require 'layout.php';
-		}
-	else{
-		require 'home.php';
-		}
-	
+$dbc = mysqli_connect('localhost', 'root', 'dexde3', 'bankman'); 
+if(!$dbc) {
+    die('DATABASE CONNECTION FAILED:'.mysqli_error($dbc));
+exit();
+}
 
+$dbs = mysqli_select_db($dbc, 'bankman');
+
+if(!$dbs) {
+    die('DATABASE SELECTION FAILED:'.mysqli_error($dbc));
+exit();
+}
+$_GET['atm'] = 0;
+$_GET['pin'] = 0;
+$user_id = mysqli_real_escape_string($dbc, $_GET['atm']);
+$psw = mysqli_real_escape_string($dbc, $_GET['pin']);
+
+$sql = $dbc->query("SELECT * FROM customers WHERE ATM_NO='$user_id' AND PIN='$psw'");
+
+ if (mysqli_num_rows($sql) > 0)
+ {
+    echo 'log in ok';
+ }
+ else {
+     echo 'log in er';
+ }
+ mysqli_close($dbc);
 ?>
-
