21f322276acaca7ad5304895f4fd4b7ede30e1a0
jestem&pro <dominikgrochowski123@wp.pl>
2021-02-14 08:46:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+    $dbname1 = 'heroku_5bed88585e7b234';
+    $dbpass = '41f08082';
+    $dbuser = 'bd78a604caf3aa';
 
-$dbc = mysqli_connect('localhost', 'root', 'dexde3', 'bankman'); 
+$dbc = mysqli_connect('us-cdbr-east-02.cleardb.com', $dbuser, $dbpass, $dbname1);
+
 if(!$dbc) {
     die('DATABASE CONNECTION FAILED:'.mysqli_error($dbc));
 exit();
